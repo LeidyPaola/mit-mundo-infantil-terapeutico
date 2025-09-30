cea3891575383b90b2b318e1e327817a1538e064
Leidy Paola <36081006+LeidyPaola@users.noreply.github.com>
2025-09-30 13:49:59 -0500

    Update ChatIA.js

--- src/components/ChatIA.js
+++ src/components/ChatIA.js
@@ -37,7 +37,7 @@ function ChatIA() {
           </div>
           <div className="flex justify-center mt-6">
           <button
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+            className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
             onClick={scrollToChat}
           >
             Ir al chat
